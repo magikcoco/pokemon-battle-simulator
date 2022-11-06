81103c42eab1df0d9a14a37c2809e9898d84c14b
homepc <punishedtomato@gmail.com>
2022-11-06 18:20:05 -0500

    Created bottomstylesheet.css, bottom-effect.png, pokemon_bw_game.ttf, top-scroll-effect.png Renamed pokemon_title.png to pokemon-title.png Added listener to scene for key presses and case for Enter key press Added better commenting to Main.java Added animated effect to the top main menu Added directional text and stylization to bottom main menu Added new font to globalstylesheet.css and set overflow properties to hidden

--- src/main/java/com/magikcoco/main/Main.java
+++ src/main/java/com/magikcoco/main/Main.java
@@ -7,43 +7,66 @@ import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import static javafx.scene.input.KeyCode.ENTER;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
         //start function
+
+        //create the root
         VBox root = new VBox();
 
-        double height = 400.0;
+        //set the size of the whole application
+        double height = 400.0; //the resolution of a 3DS top and bottom screen together
         double width = 480.0;
 
-        WebView topView = new WebView();
-        topView.prefHeight(height/2);
+        //the webview that sits on top
+        WebView topView = new WebView(); //this is the top screen
+        topView.prefHeight(height/2); //half height since there are two of these
         topView.prefWidth(width);
         WebEngine topEngine = topView.getEngine();
         topEngine.load(getClass().getResource("/mainmenu/mainmenutop.html").toString());
         root.getChildren().add(topView);
 
-        WebView bottomView = new WebView();
-        bottomView.prefHeight(height/2);
+        //the webview that sits on bottom
+        WebView bottomView = new WebView(); //this is the bottom screen
+        bottomView.prefHeight(height/2); //half height since there are two of these
         bottomView.prefWidth(width);
         WebEngine bottomEngine = bottomView.getEngine();
         bottomEngine.load(getClass().getResource("/mainmenu/mainmenubottom.html").toString());
         root.getChildren().add(bottomView);
 
+        //create the scene and add listener
+        Scene scene = new Scene(root, height, width);
+        //key bindings for events
+        scene.setOnKeyPressed(e -> {
+            if(e.getCode() == ENTER){
+                if(bottomEngine.getTitle().equals("mainmenubottom")){
+                    System.out.println("Enter Pressed");
+                }
+            }
+        });
+
+        //set the scene on the stage
+        primaryStage.setScene(scene);
+
+        //set close request action
         primaryStage.setOnCloseRequest((e)->{
             Platform.exit();
             System.exit(0);
         });
+
+        //set other elements of stage and show it
         primaryStage.setTitle("Pokemon Battle Simulator");
         primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(root, height, width));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         //main function
+        //launch method for JavaFX, blocks this thread
         launch(args);
     }
 
