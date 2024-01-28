3a77e5f185b6824a854b8e89f0b0460a2b699246
magikcoco <magikcoco.dev@gmail.com>
2024-01-28 16:41:33 -0500

    added buttons to press with console output

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-use iced::{widget::text, Sandbox, Settings};
+use iced::widget::{button, Button, Column, Text};
+use iced::{executor, Application, Command, Element, Settings, Theme};
 
 fn main() -> iced::Result{
-    Editor::run(Settings::default())
+    Simulator::run(Settings::default())
 }
 
-struct Editor;
+struct Simulator {
+    hello_world_button_state: button::State,
+    second_button_state: button::State
+}
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Debug, Clone)]
+enum Message {
+    HelloWorldButton,
+    SecondButton
+}
 
-impl Sandbox for Editor {
+impl Application for Simulator {
+    type Executor = executor::Default;
+    type Flags = ();
     type Message = Message;
+    type Theme = Theme;
 
-    fn new() -> Self {
+    fn new(_flags: ()) -> (Simulator, Command<Message>) {
         //the state to be in when the porgram starts
-        Self
+        (Simulator {
+            hello_world_button_state: button::State::new(),
+            second_button_state: button::State::new()
+        }, Command::none())
     }
 
     fn title(&self) -> String {
@@ -22,15 +35,31 @@ impl Sandbox for Editor {
         String::from("Pokemon Battle Simulator")
     }
 
-    fn update(&mut self, message: Self::Message) {
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message>{
         //handling messages (events that could change the state of the application)
         //change or update the application in response
-        match message {}
+        match _message {
+            Message::HelloWorldButton => {
+                println!("Hello World!");
+            }
+            Message::SecondButton => {
+                println!("Second Button!");
+            }
+        }
+        Command::none()
     }
 
-    fn view(&self) -> iced::Element<'_, Self::Message> {
+    fn view(&self) -> Element<Self::Message> {
         //produce the widgets that comprise the user interface
         //interacting with the elements may produce messages which are fed to update
-        text("Hello World!").into()
+        
+        //create a button and add it to the UI
+        let hellobutton = Button::new(Text::new("Hello World!")).on_press(Message::HelloWorldButton);
+        let secondbutton = Button::new(Text::new("Second button")).on_press(Message::SecondButton);
+        //arrange the button
+        Column::new()
+        .push(hellobutton)
+        .push(secondbutton)
+        .into()
     }
 }
